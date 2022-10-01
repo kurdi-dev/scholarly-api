5809d84ba8f354db64d1d6285338202301a2094f
Walid <walid.r.rashed@gmail.com>
2022-10-02 01:28:06 +0300

    fix: env and app config file

--- app/core/config.py
+++ app/core/config.py
@@ -6,8 +6,8 @@ from pydantic import AnyHttpUrl, BaseSettings, validator
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    PROJECT_NAME: str = 'scholarly-api'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost:80", "https://localhost:80", "http://localhost:8000", "https://localhost:8000","http://localhost:3000", "https://localhost:3000", "http://localhost", "https://localhost"]
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
@@ -17,11 +17,16 @@ class Settings(BaseSettings):
             return v
         raise ValueError(v)
 
-    
-
     class Config:
-        case_sensitive = True
-        env_file = ".env"
+        env_prefix = 'SCHOLARLY_'
+        fields = {
+            'PROJECT_NAME': {
+                'env': 'PROJECT_NAME',
+            },
+            'BACKEND_CORS_ORIGINS': {
+                'env': 'BACKEND_CORS_ORIGINS'
+            }
+        }
 
 
 settings = Settings()
